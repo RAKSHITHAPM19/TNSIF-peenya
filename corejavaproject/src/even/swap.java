1777d37c3cb8e1214e3dc1d712b9a12d8d58eca0
Rakshitha PM <Rakshitha PM@LAPTOP-M0FSAEQC>
2026-06-16 09:02:14 +0530

    commit

---
+++ corejavaproject/src/even/swap.java
@@ -0,0 +1,26 @@
+package even;
+import java.util.Scanner;
+public class swap {
+	
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+
+	        System.out.print("Enter first number: ");
+	        int a = sc.nextInt();
+
+	        System.out.print("Enter second number: ");
+	        int b = sc.nextInt();
+
+	        int temp;
+	        temp = a;
+	        a = b;
+	        b = temp;
+
+	        System.out.println("After swapping:");
+	        System.out.println("First number = " + a);
+	        System.out.println("Second number = " + b);
+
+	        sc.close();
+	    }
+	}
+
