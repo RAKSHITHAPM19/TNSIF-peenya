cbf72e962afced5196916438512c14c7b90e6155
Rakshitha PM <rakshithapm19@gmail.com>
2026-06-25 01:07:59 +0530

    commit

---
+++ corejavaproject/src/com/tnsif/staticprogram/staticblock.java
@@ -0,0 +1,11 @@
+package com.tnsif.staticprogram;
+// demo for static block
+public class staticblock {
+        
+        static {
+                System.out.println("welcome to java");
+        }
+        public static void main(String[] args) {
+                System.out.println("welcome to python");
+        }
+}
